3fa495b00659fc552548888e3e72b9ba43500814
David Flynn <davidf@rd.bbc.co.uk>
2012-01-12 16:13:00 +0000

    osx: delete unused code

--- src/agl_getproc.cpp
+++ src/agl_getproc.cpp
@@ -2,24 +2,6 @@
 #include <AGL/agl.h>
 #include <OpenGL/OpenGL.h>
 
-CFBundleRef gBundleRefOpenGL = NULL;
-
-void my_aglEnableMultiThreading(void)
-{
-	CGLError err = kCGLNoError;
-	CGLContextObj ctx = CGLGetCurrentContext();
-
-	// Enable the multi-threading
-	err = CGLEnable( ctx, kCGLCEMPEngine);
-
-	if (err != kCGLNoError )
-	{
-		fprintf(stderr, "Could not enable OSX multithreaded opengl");
-		// Multi-threaded execution is possibly not available
-		// Insert your code to take appropriate action
-	}
-}
-
 //nasty ugly code because including agl.h in elsewhere causes compile errors
 void my_aglSwapInterval(int interval)
 {
--- src/agl_getproc.h
+++ src/agl_getproc.h
@@ -1,7 +1,6 @@
 #ifndef AGL_GETPROC_H_
 #define AGL_GETPROC_H_
 
-void my_aglEnableMultiThreading(void);
 void my_aglSwapInterval(int interval);
 
 #endif /*AGL_GETPROC_H_*/
